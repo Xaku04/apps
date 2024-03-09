c7a0dfe1f09fe7ef62c5b2cc3e868baee7a82772
Xaku04 <119938445+Xaku04@users.noreply.github.com>
2024-03-09 00:28:23 -0800

    Add files via upload
    sakto na ang pag ascending og descending :)
    
    buttons nalang

---
+++ myapp.js
@@ -0,0 +1,158 @@
+var app = angular.module("myapp", ["ngRoute"]);
+
+app.config(function($routeProvider) {
+  $routeProvider.when("/main", {
+    templateUrl: 'main.html',
+    controller: 'mainController',
+    resolve: {
+      'check': function($rootScope, $location) {
+        if (!$rootScope.logged) {
+          $location.path("/");
+        }
+      }
+    }
+  });
+  $routeProvider.when("/", {
+    templateUrl: 'login.html',
+    controller: 'loginController',
+
+  });
+});
+
+app.controller("loginController", function($scope, $location, $rootScope) {
+
+  $scope.login = function() {
+    var uname = $scope.username;
+    var pword = $scope.password;
+    if (uname == "admin" && pword == "user") {
+      $rootScope.logged = true;
+      $location.path("/main");
+    } else {
+      $rootScope.message = "Invalid user";
+    }
+  }
+
+});
+
+app.controller("mainController", function($scope, $http, $rootScope, $location) {
+  $scope.pagesizes = [5, 10, 15, 20];
+  $scope.pageSize = 5;
+  $scope.currentPage = 0;
+
+  $scope.logout = function() {
+    $rootScope.logged = false;
+    $location.path("/");
+    $rootScope.message = "Logged Out";
+  }
+
+
+  $scope.courses = [{
+      'courseid': 'bsit',
+      'coursename': 'information technology'
+    },
+    {
+      'courseid': 'bscs',
+      'coursename': 'computer science'
+    },
+    {
+      'courseid': 'bsis',
+      'coursename': 'information systems'
+    },
+    {
+      'courseid': 'bsim',
+      'coursename': 'information management'
+    },
+    {
+      'courseid': 'act',
+      'coursename': 'computer technology'
+    },
+  ];
+
+  $scope.levels = [{
+      'levelid': '1',
+      'levelname': '1st year'
+    },
+    {
+      'levelid': '2',
+      'levelname': '2nd year'
+    },
+    {
+      'levelid': '3',
+      'levelname': '3rd year'
+    },
+    {
+      'levelid': '4',
+      'levelname': '4th year'
+    },
+  ];
+
+  $http({
+    'url': '/studentlist',
+    'method': 'get'
+  }).then(function(response) {
+    $scope.slist = response.data;
+    $scope.header = Object.keys($scope.slist[0]);
+  });
+
+  $scope.numberOfPages = function() {
+    return Math.ceil($scope.slist.length / $scope.pageSize);
+  }
+
+  $scope.modalcontrol = function(modalname, control) {
+    document.getElementById(modalname).style.display = control;
+  }
+
+  $scope.savestudent = function() {
+    $http({
+      'url': '/savestudent',
+      'method': 'post',
+      'data': {
+        'idno': $scope.idno,
+        'lastname': $scope.lastname,
+        'firstname': $scope.firstname,
+        'course': $scope.course,
+        'level': $scope.level,
+      }
+    }).then(function(response) {
+      $scope.message = response.data;
+      modalcontrol('studentmodal', 'none');
+    });
+  }
+
+  $scope.sortBy = 'lastname'; // Default sorting by LastName
+  $scope.sortAsc = true; // Default ascending
+
+  $scope.sortStudents = function(column) {
+    if ($scope.sortBy === column) {
+      $scope.sortAsc = !$scope.sortAsc;
+    } else {
+      $scope.sortBy = column;
+      $scope.sortAsc = true;
+    }
+
+    $scope.slist.sort(function(a, b) {
+      var fieldA = a[$scope.sortBy];
+      var fieldB = b[$scope.sortBy];
+      var comparison = 0;
+
+      if (typeof fieldA === 'string' && typeof fieldB === 'string') {
+        fieldA = fieldA.toUpperCase();
+        fieldB = fieldB.toUpperCase();
+      }
+
+      if (fieldA > fieldB) {
+        comparison = 1;
+      } else if (fieldA < fieldB) {
+        comparison = -1;
+      }
+      return $scope.sortAsc ? comparison : -comparison;
+    });
+  };
+});
+
+app.filter("startFrom", function() {
+  return function(input, start) {
+    start = +start;
+    return input.slice(start);
+  };
+});
